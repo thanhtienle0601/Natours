59ece81d0d60b5a9abf35642957141cddcaaca7d
thanhtienle0601 <thanhtienle0601@gmail.com>
2024-09-18 11:48:45 +0700

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -5,7 +5,7 @@ export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://localhost:8000/api/v1/users/login',
+      url: '/api/v1/users/login',
       data: {
         email,
         password,
